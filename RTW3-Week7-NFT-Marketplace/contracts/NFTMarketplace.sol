56cdc90466088441eaab3b9105dc1fcafb4c50d7
musta55 <bsse1114@iit.du.ac.bd>
2023-04-09 12:40:14 +0600

    first commit

---
+++ RTW3-Week7-NFT-Marketplace/contracts/NFTMarketplace.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extentions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+    address payable owner;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listprice = 0.01 ether;
+
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        owner = payable(msg.sender);
+    }
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;
+    }
+
+    event TokenListedSuccess(
+        uint256 indexed tokenId,
+        address owner,
+        address seller,
+        uint256 price,
+        bool currentlyListed
+    );
+
+    mapping(uint256 => ListedToken) private idToListedToken;
+
+    function updateListPrice(uint256 _listPrice) public payable {
+        require(owner == msg.sender, "only owner can update the listing price");
+        listprice = _listPrice;
+    }
+
+    function getListPrice() public view returns (uint256) {
+        return listprice;
+    }
+
+    function getLatestIdToListedToken()
+        public
+        view
+        returns (ListenToken memory)
+    {
+        uint256 currentTokenId = _tokenIds.current();
+        return idToListedToken(currentTokenId);
+    }
+
+    function getListedForTokenId(uint256 tokenId) returns (ListedToken) {
+        return idToListedToken(tokenId);
+    }
+
+    function createToken(
+        string memory tokenURI,
+        uint256 price
+    ) public payable returns (uint256) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenid);
+
+        _setTokenURI(newTokenId, tokenURI);
+
+        //Helper function to update Global variables and emit an event
+        createListedToken(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    function createListedToken(uint256 tokenid, uint256 price) private {
+        require(msg.value == listPrice, "Hopefully sending the correct price");
+        require(price > 0, "Make sure the price isn't negative");
+
+        idToListedToken[tokenId] = ListedToken(
+            tokenid,
+            payable(address(this)),
+            payable(msg.sender),
+            price,
+            true
+        );
+
+        _transfer(msg.sender, address(this), tokenid);
+
+        emit TokenListedSuccess(
+            tokenId,
+            address(this),
+            msg.sender,
+            price,
+            true
+        );
+    }
+}
